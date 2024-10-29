1e05d5e3b6dd94fab06fcc478530fc965260d353
2300614h <183996490+2300614h@users.noreply.github.com>
2024-10-29 11:49:10 +0900

    Change section3-3

--- section3-3/sketch.js
+++ section3-3/sketch.js
@@ -1,21 +1,33 @@
 // テキスト「キーボード操作に反応する」
 let x, y;
+let s;
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
   x = width / 2;
   y = height / 2;
+  s = 5
 }
+
+// function draw(){
+//   background(160, 192, 255);
+//   ellipse(x, y, 50);
+//   if(keyIsDown(LEFT_ARROW)){ x -= 5; }
+//   if(keyIsDown(RIGHT_ARROW)){ x += 5; }
+//   if(keyIsDown(UP_ARROW)){ y -= 5; }
+//   if(keyIsDown(DOWN_ARROW)){ y += 5; }
+//   if(keyIsDown("A".charCodeAt(0))){ x+= 10; }
+//   if(keyIsDown(" ".charCodeAt(0))){ x-= 10; }
 
 function draw(){
   background(160, 192, 255);
   ellipse(x, y, 50);
-  if(keyIsDown(LEFT_ARROW)){ x -= 5; }
-  if(keyIsDown(RIGHT_ARROW)){ x += 5; }
-  if(keyIsDown(UP_ARROW)){ y -= 5; }
-  if(keyIsDown(DOWN_ARROW)){ y += 5; }
-  if(keyIsDown("A".charCodeAt(0))){ x+= 10; }
-  if(keyIsDown(" ".charCodeAt(0))){ x-= 10; }
+  if(keyIsDown("A".charCodeAt(0))){ s = 10 ;}
+else{ s = 5 ;}
+  if(keyIsDown(LEFT_ARROW)){ x -= s; }
+  if(keyIsDown(RIGHT_ARROW)){ x += s; }
+  // if(keyIsDown(UP_ARROW)){ y -= s; }
+  // if(keyIsDown(DOWN_ARROW)){ y += s; }
 }
 
 // イベントハンドラを使用するパターン
@@ -30,3 +42,37 @@ function draw(){
 function windowResized(){
   resizeCanvas(windowWidth, windowHeight);
 }
+
+// let x, y;
+// let s;
+
+// function setup(){
+//   createCanvas(windowWidth, windowHeight);
+//   x = width / 2;
+//   y = height / 2;
+//   s = 5
+// }
+
+// // function draw(){
+// //   background(160, 192, 255);
+// //   ellipse(x, y, 50);
+// //   if(keyIsDown(LEFT_ARROW)){ x -= 5; }
+// //   if(keyIsDown(RIGHT_ARROW)){ x += 5; }
+// //   if(keyIsDown(UP_ARROW)){ y -= 5; }
+// //   if(keyIsDown(DOWN_ARROW)){ y += 5; }
+// //   if(keyIsDown("A".charCodeAt(0))){ x+= 10; }
+// //   if(keyIsDown(" ".charCodeAt(0))){ x-= 10; }
+
+// function draw(){
+//   background(160, 192, 255);
+//   ellipse(x, y, 50);
+//   if(keyIsDown("A".charCodeAt(0))){ s = 10 ;}
+// else{ s = 5 ;}
+//   if(keyIsDown(LEFT_ARROW)){ x -= s; }
+//   if(keyIsDown(RIGHT_ARROW)){ x += s; }
+// if(keyIsDown(" ".charCodeAt(0))){y+=}
+// // }
+
+// function windowResized(){
+//   resizeCanvas(windowWidth, windowHeight);
+// }
